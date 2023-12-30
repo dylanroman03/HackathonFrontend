42272c28d928ff3af4abec845cbb708ab6a3f8eb
Philip John <95682231+Philip-Nwabuwa@users.noreply.github.com>
2023-12-30 08:56:28 +0000

    chore: added Navbar & auth ui pages

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import { Icons } from "@/components/common/Icons";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Store } from "lucide-react";
+import Link from "next/link";
+
+const Login = () => {
+  return (
+    <section className="flex items-center justify-center h-screen">
+      <Card className="w-[400px]">
+        <CardHeader className="space-y-1">
+          <Link href={"/"} className="flex items-center justify-center gap-2 mb-4">
+            <Store />
+            <h2 className="flex text-3xl text-bold">AI-NFT</h2>
+          </Link>
+          <CardTitle className="text-2xl">Login</CardTitle>
+          <CardDescription>
+            Enter your email below to Login to your Account.
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="grid gap-4">
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input id="email" type="email" placeholder="m@example.com" />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="password">Password</Label>
+            <Input id="password" type="password" />
+          </div>
+          <Button className="w-full ">Submit</Button>
+
+          <div className="relative">
+            <div className="absolute inset-0 flex items-center">
+              <span className="w-full border-t" />
+            </div>
+            <div className="relative flex justify-center text-xs uppercase">
+              <span className="bg-background px-2 text-muted-foreground">
+                Or continue with
+              </span>
+            </div>
+          </div>
+          <div className="grid grid-cols-2 gap-6">
+            <Button variant="outline">
+              <Icons.gitHub className="mr-2 h-4 w-4" />
+              Github
+            </Button>
+            <Button variant="outline">
+              <Icons.google className="mr-2 h-4 w-4" />
+              Google
+            </Button>
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Link href={"/signup"} className="hover:underline">Dont have an account register?</Link>
+        </CardFooter>
+      </Card>
+    </section>
+  );
+};
+
+export default Login;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 const page = () => {
   return (
-    <div>page</div>
+    <section className="px-20 pt-[80px]">
+      <h1 className="text-4xl font-bold">
+        Welcome to <span className="text-blue-500">AI-NFT prediction marketplace</span>
+      </h1>
+    </section>
   )
 }
 
